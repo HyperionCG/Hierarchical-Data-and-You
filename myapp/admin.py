cfcd091eec22125a4031c56d6808989e41006fb9
Babatunde Odumosu <tudy4300@pop-os.localdomain>
2020-06-14 14:33:16 -0500

    first commit

---
+++ myapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from myapp.models import ModelName, AnotherModel
+admin.site.register(ModelName)
+admin.site.register(AnotherModel)
+
+
+# Register your models here.
---
+++ myapp/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class ModelName(models.Model):
+    field1 = models.something()
+    field2 = models.something()
